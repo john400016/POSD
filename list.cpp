5d5d416333c3e1eb391333edbe235fd2e53374eb
howlie190 <howlie190@gmail.com>
2017-10-24 17:28:31 +0800

    HW4 Done

---
+++ list.cpp
@@ -0,0 +1,61 @@
+#include "list.h"
+
+#include <string>
+using namespace std;
+
+string List::symbol() const{
+    if(!_elements.size()){
+        return "[]";
+    }
+    string ret = "[";
+    for(int i = 0; i < _elements.size() -1 ; i++){
+        ret += _elements[i]-> symbol() + ", ";
+    }
+    ret += _elements[_elements.size() -1] -> symbol() + "]";
+    return ret;
+}
+
+string List::value() const{
+    if(!_elements.size()){
+        return "[]";
+    }
+    string ret = "[";
+    for(int i = 0; i < _elements.size() -1 ; i++){
+        ret += _elements[i]-> value() + ", ";
+    }
+    ret += _elements[_elements.size() -1] -> value() + "]";
+    return ret;
+}
+
+bool List::match(Term & term){
+    List * pl = dynamic_cast<List *>(&term);
+    if (pl){
+        if(_elements.size()!= pl->_elements.size())
+            return false;
+        for(int i = 0;i < _elements.size() ;i++){
+            bool ret = _elements[i] -> match(*(pl -> _elements[i]));
+            if(!ret)
+                return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+Term * List::head() const{ 
+        if(!_elements.size())
+            throw  string("Accessing tail in an empty list");
+
+    return _elements[0];
+}
+
+List * List::tail() const{
+    if(!_elements.size())
+    throw  string("Accessing tail in an empty list");
+    std ::vector<Term *> v = {};
+    for(int i =1; i < _elements.size();i++){
+        v.push_back(_elements[i]);
+    }
+    List * l = new List(v);
+    return l;
+}
